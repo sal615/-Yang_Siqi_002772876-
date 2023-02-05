8a6fe18b408e8f4d463a46af12016f08d71cfcfe
sal615 <113734591+sal615@users.noreply.github.com>
2023-02-05 16:54:50 -0500

    upload lab work 3

---
+++ MedicineCatalog.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.ArrayList;
+import Model.Medicine;
+import Model.MedicineCatalog;
+        
+
+/**
+ *
+ * @author siqiyang
+ */
+public class MedicineCatalog {
+    ArrayList <Medicine> medicineList;
+    
+    
+    public MedicineCatalog(){
+        this.medicineList = new ArrayList<Medicine>();
+    }
+
+    public ArrayList<Medicine> getMedicineList() {
+        return medicineList;
+    }
+
+    public void setMedicineList(ArrayList<Medicine> medicineList) {
+        this.medicineList = medicineList;
+    }
+    
+    
+    public Medicine createMedicine(String name, double dosage) {
+        Medicine medicine = new Medicine();
+        medicine.setMedicineName(name);
+        medicine.setDosage(dosage);
+        
+        this.medicineList.add(medicine);
+        
+        return medicine;
+    }
+    
+    
+    public Boolean checkIfMedicineUnique(String name){
+        for(Medicine med: this.medicineList){
+            if (med.getMedicineName().equals(name)){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public void removeMedicine(String name) {
+         // find the medicine obj in the arraylist with this name
+         
+         for(Medicine med: this.medicineList) {
+             if(med.getMedicineName().equals(name)) {
+                 this.medicineList.remove(med);
+                 break;
+             }
+         }
+    }
+ }
